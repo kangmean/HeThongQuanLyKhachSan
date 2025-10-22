a746da1bc705988f60f682d36b5f43b31920f0d0
pkminh3454@gmail.com <pkminh3454@gmail.com>
2025-10-22 20:29:55 +0700

    hoan thanh phan tho cac chuc nang

--- src/hethongquanlykhachsan/HoaDonDao.java
+++ src/hethongquanlykhachsan/HoaDonDao.java
@@ -18,22 +18,35 @@ public class HoaDonDao {
      }
 
     // Tạo hóa đơn
-     public boolean themHoaDon(int maDatPhong, Date ngayLap, double tongTien) throws SQLException {
+     public boolean themHoaDon(int maKH, int maPhong, String ngayLap, double tongTien) throws SQLException {
           Connection con = connHelper.getConnection();
-          String sql = "INSERT INTO HoaDon(MaDatPhong, NgayLap, TongTien) VALUES(?,?,?)";
+          String sql = "INSERT INTO HoaDon(MaKH, MaPhong, NgayLap, TongTien) VALUES(?,?,?,?)";
           PreparedStatement pst = con.prepareStatement(sql);
-          pst.setInt(1, maDatPhong);
-          pst.setDate(2, ngayLap);
-          pst.setDouble(3, tongTien);
+          pst.setInt(1, maKH);
+          pst.setInt(2, maPhong);
+          pst.setString(3, ngayLap);
+          pst.setDouble(4, tongTien);
           return pst.executeUpdate() > 0;
-     }
+      }
 
     // Lấy hóa đơn theo đặt phòng
-     public ResultSet getHoaDonByDatPhong(int maDatPhong) throws SQLException {
+     public boolean suaHoaDon(int maHD, int maKH, int maPhong, String ngayLap, double tongTien) throws SQLException {
           Connection con = connHelper.getConnection();
-          String sql = "SELECT * FROM HoaDon WHERE MaDatPhong = ?";
+          String sql = "UPDATE HoaDon SET MaKH=?, MaPhong=?, NgayLap=?, TongTien=? WHERE MaHD=?";
           PreparedStatement pst = con.prepareStatement(sql);
-          pst.setInt(1, maDatPhong);
-          return pst.executeQuery();
+          pst.setInt(1, maKH);
+          pst.setInt(2, maPhong);
+          pst.setString(3, ngayLap);
+          pst.setDouble(4, tongTien);
+          pst.setInt(5, maHD);
+          return pst.executeUpdate() > 0;
+      }
+     
+     public boolean xoaHoaDon(int maHD) throws SQLException {
+          Connection con = connHelper.getConnection();
+          String sql = "DELETE FROM HoaDon WHERE MaHD=?";
+          PreparedStatement pst = con.prepareStatement(sql);
+          pst.setInt(1, maHD);
+          return pst.executeUpdate() > 0;
      }
 }
--- src/hethongquanlykhachsan/KhachHangDao.java
+++ src/hethongquanlykhachsan/KhachHangDao.java
@@ -12,40 +12,48 @@ public class KhachHangDao {
     // Lấy tất cả khách hàng
      public ResultSet getAllKhachHang() throws SQLException {
           Connection con = connHelper.getConnection();
-          String sql = "SELECT * FROM KhachHang";
+          String sql = "SELECT * FROM Khach";
           PreparedStatement pst = con.prepareStatement(sql);
           return pst.executeQuery();
      }
 
-     // Thêm khách hàng mới
-     public boolean themKhachHang(String tenKH, String CMND, String SDT, String email) throws SQLException {
-          Connection con = connHelper.getConnection();
-          String sql = "INSERT INTO KhachHang(TenKH, CMND, SDT) VALUES(?,?,?)";
-          PreparedStatement pst = con.prepareStatement(sql);
-          pst.setString(1, tenKH);
-          pst.setString(2, CMND);
-          pst.setString(3, SDT);
-          return pst.executeUpdate() > 0;
-     }
+     // Thêm khách hàng
+     public boolean themKhachHang(String ten, String cmnd, String sdt, int tuoi, int namSinh, String queQuan, String diaChi) throws SQLException {
+        Connection con = connHelper.getConnection();
+        String sql = "INSERT INTO Khach(TenKH, CMND, SDT, Tuoi, NamSinh, QueQuan, DiaChi) VALUES (?,?,?,?,?,?,?)";
+        PreparedStatement pst = con.prepareStatement(sql);
+        pst.setString(1, ten);
+        pst.setString(2, cmnd);
+        pst.setString(3, sdt);
+        pst.setInt(4, tuoi);
+        pst.setInt(5, namSinh);
+        pst.setString(6, queQuan);
+        pst.setString(7, diaChi);
+        return pst.executeUpdate() > 0;
+    }
 
-     // Cập nhật thông tin khách
-     public boolean capNhatKhachHang(int maKH, String tenKH, String CMND, String SDT) throws SQLException {
-         Connection con = connHelper.getConnection();
-         String sql = "UPDATE KhachHang SET TenKH=?, CMND=?, SDT=? WHERE MaKH=?";
-         PreparedStatement pst = con.prepareStatement(sql);
-         pst.setString(1, tenKH);
-         pst.setString(2, CMND);
-         pst.setString(3, SDT);
-         pst.setInt(4, maKH);
-         return pst.executeUpdate() > 0;
+     // Sửa thông tin khách hàng
+     public boolean suaKhachHang(int maKH, String ten, String cmnd, String sdt, int tuoi, int namSinh, String queQuan, String diaChi) throws SQLException {
+          Connection con = connHelper.getConnection();
+          String sql = "UPDATE Khach SET TenKH=?, CMND=?, SDT=?, Tuoi=?, NamSinh=?, QueQuan=?, DiaChi=? WHERE MaKH=?";
+          PreparedStatement pst = con.prepareStatement(sql);
+          pst.setString(1, ten);
+          pst.setString(2, cmnd);
+          pst.setString(3, sdt);
+          pst.setInt(4, tuoi);
+          pst.setInt(5, namSinh);
+          pst.setString(6, queQuan);
+          pst.setString(7, diaChi);
+          pst.setInt(8, maKH);
+          return pst.executeUpdate() > 0;
      }
 
     // Xóa khách
      public boolean xoaKhachHang(int maKH) throws SQLException {
-          Connection con = connHelper.getConnection();
-          String sql = "DELETE FROM KhachHang WHERE MaKH=?";
-          PreparedStatement pst = con.prepareStatement(sql);
-          pst.setInt(1, maKH);
-          return pst.executeUpdate() > 0;
-     }
+        Connection con = connHelper.getConnection();
+        String sql = "DELETE FROM Khach WHERE MaKH=?";
+        PreparedStatement pst = con.prepareStatement(sql);
+        pst.setInt(1, maKH);
+        return pst.executeUpdate() > 0;
+    }
 }
